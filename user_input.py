bd44f154a6af6e7e453549a066f2985dac42a31e
Kennedymbatia <mbatiakennedy66@gmail.com>
2024-03-03 11:29:36 +0300

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Ask the user for their name, age, and location
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you from? ")
+
+# Print out a personalized message
+print("Hello, {}! You are {} years old and you are from {}.".format(name, age, location))
